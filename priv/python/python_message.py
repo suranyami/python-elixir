b84b46cd15fe942537379d1772ebb6f62687a537
David Parry <david.parry@suranyami.com>
2022-06-17 10:50:49 +1000

    Add custom message

--- priv/python/python_message.py
+++ priv/python/python_message.py
@@ -1,8 +1,9 @@
 def hello(my_string):
-  if isinstance(my_string, bytes):
-    my_string = my_string.decode("utf-8")
-  
-  return "Hello world from " + my_string
+    if isinstance(my_string, bytes):
+        my_string = my_string.decode("utf-8")
+    result = "Hello world from " + my_string
+    print(result)
+    return result
 
 if __name__ == '__main__':
-  print(hello("Python"))
+    print(hello("Python"))
